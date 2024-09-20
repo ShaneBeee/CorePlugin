873d3af9d0d2019df13b1fa0f83220d75bb99933
shanebeee <shanebolenback@me.com>
2024-09-20 00:24:15 -0700

    McUtils - add some new utils

---
+++ src/main/java/com/shanebeestudios/core/api/util/McUtils.java
@@ -0,0 +1,150 @@
+package com.shanebeestudios.core.api.util;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.BiomeResolver;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import org.apache.commons.lang3.mutable.MutableInt;
+import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
+import org.bukkit.World;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Utility methods for handling conversions between Bukkit and Minecraft(NMS)
+ */
+public class McUtils {
+
+    private McUtils() {
+    }
+
+    /**
+     * Get an instance of ServerLevel from a {@link World Bukkit World}
+     *
+     * @param world World to get ServerLevel from
+     * @return ServerLevel from World
+     */
+    @NotNull
+    public static ServerLevel getServerLevel(@NotNull World world) {
+        return ((CraftWorld) world).getHandle();
+    }
+
+    /**
+     * Get a Minecraft BlockPos from a Bukkit Location
+     *
+     * @param location Location to change to BlockPos
+     * @return BlockPos from Location
+     */
+    @NotNull
+    public static BlockPos getPos(@NotNull Location location) {
+        return new BlockPos(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+    }
+
+    /**
+     * Convert Minecraft ResourceLocation to Bukkit NamespacedKey
+     *
+     * @param resourceLocation ResourceLocation to change to NamespacedKey
+     * @return ResourceLocation from NamespacedKey
+     */
+    @NotNull
+    public static NamespacedKey getNamespacedKey(ResourceLocation resourceLocation) {
+        return new NamespacedKey(resourceLocation.getNamespace(), resourceLocation.getPath());
+    }
+
+    /**
+     * Convert a Bukkit NamespacedKey to Minecraft ResourceLocation
+     *
+     * @param bukkitKey NamespacedKey to change to ResourceLocation
+     * @return ResourceLocation from NamespacedKey
+     */
+    @NotNull
+    public static ResourceLocation getResourceLocation(NamespacedKey bukkitKey) {
+        return ResourceLocation.fromNamespaceAndPath(bukkitKey.getNamespace(), bukkitKey.getKey());
+    }
+
+    /**
+     * Get a holder reference from a registry
+     *
+     * @param registry Registry to grab holder from
+     * @param key      Key of holder
+     * @param <T>      Class type of registry
+     * @return Holder from registry
+     */
+    @Nullable
+    public static <T> Holder.Reference<T> getHolderReference(Registry<T> registry, NamespacedKey key) {
+        ResourceLocation resourceLocation = McUtils.getResourceLocation(key);
+        ResourceKey<T> resourceKey = ResourceKey.create(registry.key(), resourceLocation);
+        try {
+            return registry.getHolderOrThrow(resourceKey);
+        } catch (IllegalStateException ignore) {
+            return null;
+        }
+    }
+
+    /**
+     * Get a Minecraft Registry
+     *
+     * @param registry ResourceKey of registry
+     * @param <T>      ResourceKey
+     * @return Registry from key
+     */
+    public static <T> Registry<T> getRegistry(ResourceKey<? extends Registry<? extends T>> registry) {
+        return MinecraftServer.getServer().registryAccess().registryOrThrow(registry);
+    }
+
+    /**
+     * Get all keys from a registry
+     *
+     * @param registry Registry to grab keys from
+     * @param <T>      Registry class type
+     * @return List of NamespacedKeys for all keys in registry
+     */
+    @NotNull
+    public static <T> List<NamespacedKey> getRegistryKeys(Registry<T> registry) {
+        List<NamespacedKey> keys = new ArrayList<>();
+        registry.keySet().forEach(resourceLocation -> {
+            NamespacedKey namespacedKey = McUtils.getNamespacedKey(resourceLocation);
+            keys.add(namespacedKey);
+        });
+        return keys.stream().sorted(Comparator.comparing(NamespacedKey::toString)).collect(Collectors.toList());
+    }
+
+    /**
+     * Make a resolver for 3D shifted biomes
+     *
+     * @param count       counter
+     * @param chunkAccess Chunk where biome is
+     * @param box         BoundingBox for biome change
+     * @param biome       Biome
+     * @param filter      Filter
+     * @return Biome resolver
+     */
+    @NotNull
+    public static BiomeResolver getBiomeResolver(MutableInt count, ChunkAccess chunkAccess, BoundingBox box, Holder<Biome> biome, Predicate<Holder<Biome>> filter) {
+        return (x, y, z, noise) -> {
+            Holder<Biome> biomeHolder = chunkAccess.getNoiseBiome(x, y, z);
+            if (box.isInside(x << 2, y << 2, z << 2) && filter.test(biomeHolder)) {
+                count.increment();
+                return biome;
+            } else {
+                return biomeHolder;
+            }
+        };
+    }
+
+}
--- src/main/java/com/shanebeestudios/core/api/util/WorldUtils.java
+++ src/main/java/com/shanebeestudios/core/api/util/WorldUtils.java
@@ -2,19 +2,39 @@ package com.shanebeestudios.core.api.util;
 
 import com.shanebeestudios.core.plugin.CorePlugin;
 import net.kyori.adventure.util.TriState;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.core.SectionPos;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.status.ChunkStatus;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.mutable.MutableInt;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * General utility class for {@link World worlds}
  */
 @SuppressWarnings("unused")
 public class WorldUtils {
+
+    private static final Registry<Biome> BIOME_REGISTRY = McUtils.getRegistry(Registries.BIOME);
 
     private WorldUtils() {
     }
@@ -73,6 +93,57 @@ public class WorldUtils {
                 }
             });
         }
+    }
+
+    /**
+     * Get a list of all available biomes as {@link NamespacedKey NamespacedKeys}
+     * Includes custom biomes as well
+     *
+     * @return List of biomes
+     */
+    @NotNull
+    public static List<NamespacedKey> getBiomeKeys() {
+        return McUtils.getRegistryKeys(BIOME_REGISTRY);
+    }
+
+    /**
+     * Fill a Biome between 2 locations with an option to only replace a specific Biome.
+     * <p>Will also send biome updates to players.</p>
+     *
+     * @param location   First corner
+     * @param location2  Second corner
+     * @param biomeKey   Key of biome
+     * @param replaceKey Key of biome to replace
+     */
+    public static void fillBiome(@NotNull Location location, @NotNull Location location2, @NotNull NamespacedKey biomeKey, @Nullable NamespacedKey replaceKey) {
+        World world = location.getWorld();
+        if (world != location2.getWorld()) {
+            throw new IllegalArgumentException("Worlds for both locations do not match!");
+        }
+
+        BlockPos blockPos = McUtils.getPos(location);
+        BlockPos blockPos2 = McUtils.getPos(location2);
+        BoundingBox box = BoundingBox.fromCorners(blockPos, blockPos2);
+        ServerLevel level = McUtils.getServerLevel(world);
+
+        Holder.Reference<Biome> biome = McUtils.getHolderReference(BIOME_REGISTRY, biomeKey);
+        ResourceLocation replaceBiome = replaceKey != null ? McUtils.getResourceLocation(replaceKey) : null;
+        if (biome == null) return;
+
+        List<ChunkAccess> chunkAccessList = new ArrayList<>();
+        for (int z = SectionPos.blockToSectionCoord(box.minZ()); z <= SectionPos.blockToSectionCoord(box.maxZ()); ++z) {
+            for (int x = SectionPos.blockToSectionCoord(box.minX()); x <= SectionPos.blockToSectionCoord(box.maxX()); ++x) {
+                ChunkAccess chunkAccess = level.getChunk(x, z, ChunkStatus.FULL, false);
+                if (chunkAccess != null) chunkAccessList.add(chunkAccess);
+            }
+        }
+
+        for (ChunkAccess chunkAccess : chunkAccessList) {
+            chunkAccess.fillBiomesFromNoise(McUtils.getBiomeResolver(new MutableInt(0), chunkAccess, box, biome,
+                biomeHolder -> replaceBiome == null || biomeHolder.is(replaceBiome)), level.getChunkSource().randomState().sampler());
+            chunkAccess.setUnsaved(true);
+        }
+        level.getChunkSource().chunkMap.resendBiomesForChunks(chunkAccessList);
     }
 
 }
