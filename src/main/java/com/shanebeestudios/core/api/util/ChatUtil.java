b513d9ddd1796178d3457aa4751ef0942a9c8528
shanebeee <shanebolenback@me.com>
2024-09-21 23:38:36 -0700

    ChatUtil - clear parts of list over 100

--- src/main/java/com/shanebeestudios/core/api/util/ChatUtil.java
+++ src/main/java/com/shanebeestudios/core/api/util/ChatUtil.java
@@ -13,10 +13,22 @@ public class ChatUtil {
 
     private static final List<SignedMessage> MESSAGES = new ArrayList<>();
 
+    /**
+     * Log a message for clearing later
+     *
+     * @param message SignedMessage to log
+     */
     public static void logMessage(SignedMessage message) {
         MESSAGES.addFirst(message);
+        // Chat only holds 100 messages, lets not let this list get too big
+        if (MESSAGES.size() > 100) {
+            MESSAGES.subList(100, MESSAGES.size()).clear();
+        }
     }
 
+    /**
+     * Clear signed messages from chat
+     */
     public static void clearChat() {
         int length = Math.min(MESSAGES.size(), 100);
         Bukkit.getOnlinePlayers().forEach(player -> {
