45c29e950dada9f22ef33dbc1589586479611617
ShaneBeee <shanebolenback@me.com>
2025-02-11 19:52:44 -0800

    DumpRegistryCommand - add DumpRegistry to handle this better

---
+++ src/main/java/com/shanebeestudios/core/api/registry/DumpRegistry.java
@@ -0,0 +1,106 @@
+package com.shanebeestudios.core.api.registry;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.JsonOps;
+import com.shanebeestudios.core.plugin.CorePlugin;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.RegistryOps;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.level.biome.Biome;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Utility to dump registry objects to file
+ * <p>Will output to json Datapack format</p>
+ *
+ * @param <N> Registry type
+ */
+@SuppressWarnings({"unused"})
+public class DumpRegistry<N> {
+
+    private static final RegistryOps<JsonElement> REGISTRY_OPS = RegistryOps.create(JsonOps.INSTANCE, MinecraftServer.getServer().registryAccess());
+    private static final File DATA_FOLDER = CorePlugin.getInstance().getDataFolder();
+    private static final Map<String, DumpRegistry<?>> BY_NAME = new HashMap<>();
+
+    static {
+        register("biomes", Registries.BIOME, Biome.DIRECT_CODEC);
+        register("enchantments", Registries.ENCHANTMENT, Enchantment.DIRECT_CODEC);
+    }
+
+    private static <N> void register(String name, ResourceKey<Registry<N>> registry, Codec<N> codec) {
+        DumpRegistry<N> reg = new DumpRegistry<>(registry, codec);
+        BY_NAME.put(name, reg);
+    }
+
+    /**
+     * Get a DumpRegistry by name
+     *
+     * @param name Name of DumpRegistry
+     * @return DumpRegistry by name or null if none found
+     */
+    public static DumpRegistry<?> getByName(String name) {
+        return BY_NAME.get(name);
+    }
+
+    /**
+     * Get all DumpRegistry names
+     *
+     * @return All names
+     */
+    public static Set<String> getRegistryNames() {
+        return BY_NAME.keySet();
+    }
+
+    private final ResourceKey<Registry<N>> registryKey;
+    private final String registryPath;
+    private final Codec<N> codec;
+
+    private DumpRegistry(ResourceKey<Registry<N>> registryKey, Codec<N> codec) {
+        this.registryKey = registryKey;
+        this.registryPath = registryKey.location().getPath();
+        this.codec = codec;
+    }
+
+    /**
+     * Dump all objects from registry to file
+     */
+    public void dumpObjects() {
+        Registry<N> registry = MinecraftServer.getServer().registryAccess().lookupOrThrow(registryKey);
+        for (N registryValue : registry) {
+            ResourceLocation key = registry.getKey(registryValue);
+            assert key != null;
+            dump(registryValue, key);
+        }
+    }
+
+    private void dump(N registryValue, ResourceLocation location) {
+        File file = new File(DATA_FOLDER, "data/" + location.getNamespace() + "/" + this.registryPath + "/" + location.getPath() + ".json");
+
+        File parent = file.getParentFile();
+        if (parent != null && !parent.exists() && !parent.mkdirs()) {
+            return;
+        }
+        DataResult<JsonElement> jsonData = this.codec.encodeStart(REGISTRY_OPS, registryValue);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try {
+            Files.writeString(file.toPath(), gson.toJson(jsonData.getOrThrow()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- src/main/java/com/shanebeestudios/core/plugin/command/DumpRegistryCommand.java
+++ src/main/java/com/shanebeestudios/core/plugin/command/DumpRegistryCommand.java
@@ -1,25 +1,9 @@
 package com.shanebeestudios.core.plugin.command;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.mojang.serialization.DataResult;
-import com.mojang.serialization.JsonOps;
-import com.shanebeestudios.core.plugin.CorePlugin;
-import com.shanebeestudios.coreapi.util.Utils;
+import com.shanebeestudios.core.api.registry.DumpRegistry;
 import dev.jorel.commandapi.CommandTree;
+import dev.jorel.commandapi.arguments.ArgumentSuggestions;
 import dev.jorel.commandapi.arguments.StringArgument;
-import net.minecraft.core.Holder;
-import net.minecraft.resources.RegistryOps;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.level.biome.Biome;
-import org.bukkit.NamespacedKey;
-import org.bukkit.Registry;
-import org.bukkit.craftbukkit.block.CraftBiome;
-
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class DumpRegistryCommand {
 
@@ -30,46 +14,17 @@ public class DumpRegistryCommand {
     private void registerCommand() {
         CommandTree command = new CommandTree("dumpregistry")
             .then(new StringArgument("registry")
+                .includeSuggestions(ArgumentSuggestions.strings(DumpRegistry.getRegistryNames()))
                 .executes(context -> {
                     String reg = context.args().getByClass("registry", String.class);
                     assert reg != null;
-                    if (reg.equalsIgnoreCase("biome")) {
-                        dumpBiomes();
+                    DumpRegistry<?> dumpRegistry = DumpRegistry.getByName(reg);
+                    if (dumpRegistry != null) {
+                        dumpRegistry.dumpObjects();
                     }
                 }));
 
         command.register();
-    }
-
-    @SuppressWarnings("deprecation")
-    private void dumpBiomes() {
-        Utils.log("Dumping Biomes to directory!");
-        for (org.bukkit.block.Biome bukkitBiome : Registry.BIOME) {
-            NamespacedKey namespacedKey = bukkitBiome.getKey();
-
-            File file = new File(CorePlugin.getInstance().getDataFolder(), "biomes/" + namespacedKey.namespace() + "/" + namespacedKey.getKey() + ".json");
-            File parent = file.getParentFile();
-            if (parent != null && !parent.exists() && !parent.mkdirs()) {
-                continue;
-            }
-            Biome biome = CraftBiome.bukkitToMinecraft(bukkitBiome);
-            Utils.log("Creating json for biome '" + namespacedKey + "'");
-            dumpBiome(Holder.direct(biome), file.toPath());
-        }
-        Utils.log("Done dumping Biomes to directory!");
-    }
-
-    private void dumpBiome(Holder<Biome> biomeHolder, Path path) {
-        RegistryOps<JsonElement> ops = RegistryOps.create(JsonOps.INSTANCE, MinecraftServer.getServer().registryAccess());
-        DataResult<JsonElement> jsonData = Biome.CODEC.encodeStart(ops, biomeHolder);
-        JsonElement jsonElement = jsonData.getOrThrow();
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-        try {
-            Files.writeString(path, gson.toJson(jsonElement));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 
 }
