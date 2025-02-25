024f208f6fa6af99e61c26bc6a3873ed5fb10fee
ShaneBeee <shanebolenback@me.com>
2025-02-24 16:35:10 -0800

    StatsRamBar - switch to adventure bossbar

--- src/main/java/com/shanebeestudios/core/plugin/stats/StatsRamBar.java
+++ src/main/java/com/shanebeestudios/core/plugin/stats/StatsRamBar.java
@@ -3,25 +3,22 @@ package com.shanebeestudios.core.plugin.stats;
 import com.shanebeestudios.core.api.util.Permissions;
 import com.shanebeestudios.coreapi.util.TaskUtils;
 import com.shanebeestudios.coreapi.util.Utils;
+import net.kyori.adventure.bossbar.BossBar;
 import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
-import org.bukkit.boss.BarColor;
-import org.bukkit.boss.BarStyle;
-import org.bukkit.boss.KeyedBossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.util.stream.StreamSupport;
+
 public class StatsRamBar implements Listener, Stats {
 
-    private final KeyedBossBar bossbar;
+    private final BossBar bossbar;
 
     public StatsRamBar() {
-        NamespacedKey key = NamespacedKey.fromString("core:rambar");
-        assert key != null;
-        this.bossbar = Bukkit.createBossBar(key, "RamBar", BarColor.BLUE, BarStyle.SEGMENTED_20);
+        this.bossbar = BossBar.bossBar(Utils.getMini("RamBar"), 1.0f, BossBar.Color.WHITE, BossBar.Overlay.NOTCHED_20);
         Bukkit.getOnlinePlayers().forEach(player -> {
             if (Permissions.STATS_RAMBAR.hasPermission(player)) {
                 enable(player);
@@ -30,7 +27,6 @@ public class StatsRamBar implements Listener, Stats {
         startTimer();
     }
 
-    @SuppressWarnings("deprecation")
     private void startTimer() {
         Runtime runtime = Runtime.getRuntime();
         TaskUtils.runTaskTimerAsynchronously(() -> {
@@ -38,26 +34,28 @@ public class StatsRamBar implements Listener, Stats {
             long free = runtime.freeMemory() / 1024 / 1024;
             double used = Math.floor(max - free);
             String title = String.format("<grey>Ram: <aqua>%s<grey>mb / <aqua>%s<grey>mb", used, max);
-            this.bossbar.setTitle(Utils.getColString(title));
-            double progress = (used / max);
-            this.bossbar.setProgress(progress);
-            if (progress > 0.9) this.bossbar.setColor(BarColor.RED);
-            else if (progress > 0.75) this.bossbar.setColor(BarColor.YELLOW);
-            else this.bossbar.setColor(BarColor.GREEN);
+            this.bossbar.name(Utils.getMini(title));
+            float progress = (float) (used / max);
+            this.bossbar.progress(progress);
+            if (progress > 0.9) this.bossbar.color(BossBar.Color.RED);
+            else if (progress > 0.75) this.bossbar.color(BossBar.Color.YELLOW);
+            else this.bossbar.color(BossBar.Color.GREEN);
 
         }, 5, 5);
     }
 
     public void enable(Player player) {
-        this.bossbar.addPlayer(player);
+        this.bossbar.addViewer(player);
     }
 
     public void disable(Player player) {
-        this.bossbar.removePlayer(player);
+        this.bossbar.removeViewer(player);
     }
 
     public void toggle(Player player) {
-        if (this.bossbar.getPlayers().contains(player)) {
+        boolean hasPlayer = StreamSupport.stream(this.bossbar.viewers().spliterator(), false)
+            .anyMatch(viewer -> viewer.equals(player));
+        if (hasPlayer) {
             disable(player);
         } else {
             enable(player);
@@ -65,8 +63,7 @@ public class StatsRamBar implements Listener, Stats {
     }
 
     public void unload() {
-        this.bossbar.removeAll();
-        Bukkit.removeBossBar(this.bossbar.getKey());
+        // Doesn't need anything
     }
 
     // Listeners
